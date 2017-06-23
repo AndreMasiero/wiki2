470f2ee0d7b545ef622dcf56e2e88948749ad091
AndreMasiero <andreboavista@gmail.com>
2017-06-23 15:07:15 -0300

    usuário e senha direto no codigo

--- src/main/java/br/com/wiki/config/SpringSecurityConfig.java
+++ src/main/java/br/com/wiki/config/SpringSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.com.wiki.service.UsuarioLogadoDetailService;
 
-@Configuration
+//@Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
