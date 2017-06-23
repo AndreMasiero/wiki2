24694ff569527664db931718653ef0e796c24db2
AndreMasiero <andreboavista@gmail.com>
2017-06-23 17:13:14 -0300

    teste conexão

--- src/main/java/br/com/wiki/config/SpringDataConfig.java
+++ src/main/java/br/com/wiki/config/SpringDataConfig.java
@@ -20,12 +20,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories("br.com.wiki.repository")
 @EnableTransactionManagement
-@PropertySource("classpath:/application.properties")
+//@PropertySource("classpath:/application.properties")
 public class SpringDataConfig {
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Bean
 	public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
 		JpaTransactionManager manager = new JpaTransactionManager();
@@ -33,7 +33,7 @@ public class SpringDataConfig {
 		manager.setJpaDialect(new HibernateJpaDialect());
 		return manager;
 	}
-	
+
 	@Bean
 	public HibernateJpaVendorAdapter jpaVendorAdapter() {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
@@ -41,33 +41,41 @@ public class SpringDataConfig {
 		adapter.setGenerateDdl(env.getProperty("hibernate.ddl", Boolean.class));
 		return adapter;
 	}
-	
+
 	@Bean
-	public EntityManagerFactory entityManagerFactory() {		
+	public EntityManagerFactory entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 		factory.setJpaVendorAdapter(jpaVendorAdapter());
-		factory.setPackagesToScan(
-				env.getProperty("hibernate.package.scan"),
-				env.getProperty("java.time.jpa.converter")
-		);
+		factory.setPackagesToScan(env.getProperty("hibernate.package.scan"),
+				env.getProperty("java.time.jpa.converter"));
 		factory.setDataSource(dataSource());
-		factory.afterPropertiesSet();		
+		factory.afterPropertiesSet();
 		return factory.getObject();
 	}
-	
+
 	@Bean(name = "dataSource")
-	public DataSource dataSource() {
-		System.out.println("AQUI");
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setUsername(env.getProperty("jdbc.user"));
-		dataSource.setPassword(env.getProperty("jdbc.pass"));
-		dataSource.setDriverClassName(env.getProperty("jdbc.driverClass"));
-		dataSource.setUrl(env.getProperty("jdbc.url"));
-		
-//		dataSource.setUsername("wiki");
-//		dataSource.setPassword("Batata#14");
-//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/wiki");
-		return dataSource;
+	public BasicDataSource dataSource() {
+//		System.out.println("AQUI");
+//		BasicDataSource dataSource = new BasicDataSource();
+//		dataSource.setUsername(env.getProperty("jdbc.user"));
+//		dataSource.setPassword(env.getProperty("jdbc.pass"));
+//		dataSource.setDriverClassName(env.getProperty("jdbc.driverClass"));
+//		dataSource.setUrl(env.getProperty("jdbc.url"));
+
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+		String name = "mytomcatapp";
+		String url = "jdbc:mysql://" + host + ":" + port + "/" + name;
+		ds.setUrl(url);
+		ds.setUsername("username");
+		ds.setPassword("pwd");
+
+		// dataSource.setUsername("wiki");
+		// dataSource.setPassword("Batata#14");
+		// dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		// dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/wiki");
+		return ds;
 	}
 }
